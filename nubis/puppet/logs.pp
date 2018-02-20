51175e5b5bb2349ccb32d0425aae8273c035827a
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-02-20 11:43:13 -0500

    logs++

--- nubis/puppet/logs.pp
+++ nubis/puppet/logs.pp
@@ -1,10 +1,13 @@
+class { 'fluentd':
+  service_ensure => stopped
+}
+
 fluentd::configfile { $project_name: }
 
 fluentd::source { 'node-output':
   configfile  => $project_name,
   type        => 'tail',
-  format      => 'none',
-  time_format => '%Y-%m-%dT%H:%M:%S.%L%Z',
+  format      => 'none ',
 
   tag         => 'forward.voice.node.stdout',
   config      => {
