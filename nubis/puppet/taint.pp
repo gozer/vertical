da2f1d49e556fcb72b73af82026370a4c1b4fdf4
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-04-11 10:57:05 -0400

    Upgrade to Nubis v2.2.0

--- nubis/puppet/taint.pp
+++ nubis/puppet/taint.pp
@@ -1,24 +1,3 @@
-# Overloaded Wrapper to actually perform the tainting conditionally
-# These should be part of nubis-base
-
-# https://github.com/nubisproject/nubis-base/issues/791
-file {'/usr/local/sbin/nubis-taint':
-  ensure => file,
-  owner  => root,
-  group  => root,
-  mode   => '0755',
-  source => 'puppet:///nubis/files/nubis-taint',
-}
-
-# https://github.com/nubisproject/nubis-base/issues/793
-file {'/usr/local/sbin/nubis-taint-reap':
-  ensure => file,
-  owner  => root,
-  group  => root,
-  mode   => '0755',
-  source => 'puppet:///nubis/files/nubis-taint-reap',
-}
-
 # Disable instance tainted for this system automation account
 file { '/home/dbadmin/.no-taint':
   ensure  => file,
