51288b65e72a1cdad55133cfb0d39ad20f5ea86a
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-09-18 14:09:19 -0400

    use name not ARN

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -628,7 +628,7 @@ resource "aws_lb_target_group" "public" {
 }
 
 resource "aws_autoscaling_attachment" "worker_0" {
-  autoscaling_group_name = "${module.worker_0.autoscaling_group_arn}"
+  autoscaling_group_name = "${module.worker_0.autoscaling_group}"
   alb_target_group_arn   = "${aws_lb_target_group.public.arn}"
 }
 
